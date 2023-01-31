0960ea7a637da252455eb0b88c21f673482a14b0
Rudraz <Rudraz@users.noreply.github.com>
2023-01-31 15:55:58 +0800

    new changes

--- js/app.js
+++ js/app.js
@@ -23,6 +23,16 @@
  * 
 */
 
+//navigation global variables
+const navigation = document.getElementById('navbar__list');
+
+
+/**
+ * sections global variables
+ */
+const sections = document.querySelectorAll('section');
+
+
 
 /**
  * End Global Variables
@@ -39,7 +49,25 @@
 */
 
 // build the nav
+const buildnav = {} => {
+    
+    let navUI = '';
+    // looping over all sections based on the "querySelectorAll" in the sections global variables above 
+    sections.forEach(section => {
 
+        const sectionID = section.id;
+        const sectionDataNav = section.dataset.nav;
+
+        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+
+    });
+    // append all html elements to the navigation
+    navigation.innerHTML = navUI;
+
+
+}
+
+buildnav();
 
 // Add class 'active' to section when near top of viewport
 
